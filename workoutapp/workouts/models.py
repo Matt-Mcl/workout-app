5639a53cd8f03c1f418895c48a317344537e247a
Matt-Mcl <mattmcl2000@gmail.com>
2022-03-03 12:01:57 +0000

    Changes more fields to be optional to allow API

--- workoutapp/workouts/models.py
+++ workoutapp/workouts/models.py
@@ -7,15 +7,15 @@ class Walk(models.Model):
         ('O', 'Outdoor'),
         ('I', 'Indoor')
     ]
-    location = models.CharField(max_length=1, choices=LOCATION_CHOICES)
+    location = models.CharField(null=True, blank=True, max_length=1, choices=LOCATION_CHOICES)
     start_time = models.DateTimeField()
     duration = models.DurationField()
     distance = models.DecimalField(max_digits=6, decimal_places=2)
     actual_distance = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
-    active_kilocalories = models.IntegerField()
+    active_kilocalories = models.IntegerField(null=True, blank=True)
     elevation_gain = models.IntegerField(null=True, blank=True)
     total_kilocalories = models.IntegerField()
-    average_heart_rate = models.IntegerField()
+    average_heart_rate = models.IntegerField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
@@ -24,14 +24,14 @@ class Run(models.Model):
         ('O', 'Outdoor'),
         ('I', 'Indoor')
     ]
-    location = models.CharField(max_length=1, choices=LOCATION_CHOICES)
+    location = models.CharField(null=True, blank=True, max_length=1, choices=LOCATION_CHOICES)
     start_time = models.DateTimeField()
     duration = models.DurationField()
     distance = models.DecimalField(max_digits=6, decimal_places=2)
     actual_distance = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
-    active_kilocalories = models.IntegerField()
+    active_kilocalories = models.IntegerField(null=True, blank=True)
     elevation_gain = models.IntegerField(null=True, blank=True)
     total_kilocalories = models.IntegerField()
-    average_heart_rate = models.IntegerField()
-    average_cadence = models.IntegerField()
+    average_heart_rate = models.IntegerField(null=True, blank=True)
+    average_cadence = models.IntegerField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
