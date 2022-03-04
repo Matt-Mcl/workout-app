34964d96db1cc778f641060a87baa05225dfd5f1
Matt-Mcl <mattmcl2000@gmail.com>
2022-03-04 11:01:27 +0000

    Keeps seconds when updating walks and runs

--- workoutapp/workouts/forms.py
+++ workoutapp/workouts/forms.py
@@ -10,7 +10,7 @@ class WalkForm(forms.ModelForm):
         fields = ('start_time', 'location', 'duration', 'distance', 'actual_distance', 'active_kilocalories', 'total_kilocalories', 'elevation_gain', 'average_heart_rate')
 
         widgets = {
-            'start_time': forms.DateTimeInput(format=('%Y-%m-%dT%H:%M'), attrs={'type': 'datetime-local'}),
+            'start_time': forms.DateTimeInput(format=('%Y-%m-%dT%H:%M:%S'), attrs={'type': 'datetime-local'}),
             'duration': forms.TextInput(attrs={"class": "html-duration-picker"}),
         }
 
@@ -22,6 +22,6 @@ class RunForm(forms.ModelForm):
         fields = ('start_time', 'location', 'duration', 'distance', 'actual_distance', 'active_kilocalories', 'total_kilocalories', 'elevation_gain', 'average_heart_rate', 'average_cadence')
 
         widgets = {
-            'start_time': forms.DateTimeInput(format=('%Y-%m-%dT%H:%M'), attrs={'type': 'datetime-local'}),
+            'start_time': forms.DateTimeInput(format=('%Y-%m-%dT%H:%M:%S'), attrs={'type': 'datetime-local'}),
             'duration': forms.TextInput(attrs={"class": "html-duration-picker"}),
         }
