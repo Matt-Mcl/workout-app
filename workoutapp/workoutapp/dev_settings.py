071535488be0abc80721db20ae4c6aa1bbc324d0
Matt-Mcl <mattmcl2000@gmail.com>
2022-06-02 20:00:50 +0000

    Reverts MongoDB

--- workoutapp/workoutapp/dev_settings.py
+++ workoutapp/workoutapp/dev_settings.py
@@ -87,17 +87,10 @@ WSGI_APPLICATION = 'workoutapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'django_workoutapp_dev',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
