e6b3d2010cfcf90c5b87177d8250dd810fba4517
Matt-Mcl <mattmcl2000@gmail.com>
2024-03-18 11:45:55 +0000

    Updates workout model to follow new API

--- workoutapp/workouts/models.py
+++ workoutapp/workouts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Workout(models.Model):
     LOCATION_CHOICES = [
         ('O', 'Outdoor'),
@@ -10,22 +9,19 @@ class Workout(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=1, choices=LOCATION_CHOICES)
     start_time = models.DateTimeField(unique=True)
+    end_time = models.DateTimeField(null=True, blank=True)
     duration = models.DurationField()
     active_kilocalories = models.IntegerField()
-    total_kilocalories = models.IntegerField()
     average_heart_rate = models.IntegerField()
     max_heart_rate = models.IntegerField()
+    mins_at_hr = models.CharField(null=True, blank=True, max_length=1000)
     distance = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
     actual_distance = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
-    elevation_gain = models.IntegerField(null=True, blank=True)
+    step_count = models.IntegerField(null=True, blank=True)
     step_cadence = models.IntegerField(null=True, blank=True)
     temperature = models.IntegerField(null=True, blank=True)
     humidity = models.IntegerField(null=True, blank=True)
     intensity = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
-    flights_climbed = models.IntegerField(null=True, blank=True)
-    step_count = models.IntegerField(null=True, blank=True)
-    swim_cadence = models.IntegerField(null=True, blank=True)
-    total_swimming_stroke_count = models.IntegerField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- workoutapp/workouts/serializers.py
+++ workoutapp/workouts/serializers.py
@@ -7,7 +7,7 @@ from .models import Workout, User
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
-        fields = ('name', 'location', 'start_time', 'duration', 'active_kilocalories', 'total_kilocalories', 'average_heart_rate', 'max_heart_rate', 'distance', 'actual_distance', 'elevation_gain', 'step_cadence', 'temperature', 'humidity', 'intensity', 'flights_climbed', 'step_count', 'swim_cadence', 'total_swimming_stroke_count', 'notes', 'user')
+        fields = ('name', 'location', 'start_time', 'end_time', 'duration', 'active_kilocalories', 'average_heart_rate', 'max_heart_rate', 'distance', 'actual_distance', 'step_count', 'step_cadence', 'temperature', 'humidity', 'intensity', 'notes', 'user')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
