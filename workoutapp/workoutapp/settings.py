ec5b92b39ec51e8af16d3fd19f7e4e90b87c22b4
Matt-Mcl <mattmcl2000@gmail.com>
2024-04-03 13:20:23 +0000

    Changes static file options for production

--- workoutapp/workoutapp/settings.py
+++ workoutapp/workoutapp/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/workoutapp')
 STATIC_URL = '/static/workoutapp/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/workoutapp'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/workoutapp'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
